2df5dd4d0e17ba1ac06bc0332f2d9601bf3205db
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-04-02 01:36:37 +0900

    [WIP] Add bmpcode::read_code

--- src/bmpcode.rs
+++ src/bmpcode.rs
@@ -1,1 +1,14 @@
 extern crate bmp;
+
+
+pub fn read_code(file: &str, codel_size: u32) {
+    assert!(codel_size > 0);
+    let img = bmp::open(file).unwrap_or_else(|e| {
+                                                 panic!("Failed to open bmp file: {}", e);
+                                             });
+
+    let (width, height) = (img.get_width(), img.get_height());
+    if (width % codel_size != 0) | (height % codel_size != 0) {
+        panic!("Bad codel size: {} ({}x{})", codel_size, width, height);
+    };
+}
