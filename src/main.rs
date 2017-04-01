4eba3d4d8872f04941718eb37417293c47243d85
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-04-01 21:38:37 +0900

    Parse command-line arguments and print them
    Get input/output file name from command line arguments and
    print them.
    
    Print usage when either of them is missing.
    
    Copy from the document of getopts:
    https://doc.rust-lang.org/getopts/getopts/index.html

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate getopts;
 
 use getopts::Options;
+use std::env;
 
 
 fn print_usage(program: &str, opts: Options) {
@@ -9,5 +10,28 @@ fn print_usage(program: &str, opts: Options) {
 }
 
 fn main() {
-    println!("Hello, world!");
+    let mut opts = Options::new();
+    opts.optopt("o", "", "output file name", "NAME");
+
+    let args: Vec<String> = env::args().collect();
+    let program = args[0].clone();
+    let matches = match opts.parse(&args[1..]) {
+        Ok(m) => m,
+        Err(f) => panic!(f.to_string()),
+    };
+
+    let output = match matches.opt_str("o") {
+        Some(file) => file,
+        None => {
+            print_usage(&program, opts);
+            return;
+        }
+    };
+    let input = if !matches.free.is_empty() {
+        matches.free[0].clone()
+    } else {
+        print_usage(&program, opts);
+        return;
+    };
+    println!("input: {}, output: {}", input, output);
 }
