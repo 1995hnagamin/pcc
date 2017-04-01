a0058c25d5014a3e08a8f9244601684f56d9ef63
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-04-02 02:14:40 +0900

    Add get_codel to bmpcode.rs

--- src/bmpcode.rs
+++ src/bmpcode.rs
@@ -1,12 +1,28 @@
 extern crate bmp;
 
+use std::result;
+
+
+type ResultCodel = result::Result<bmp::Pixel, (u32, u32)>;
+
+fn get_codel(img: &bmp::Image, x: u32, y: u32, codel_size: u32) -> ResultCodel {
+    let base = img.get_pixel(x * codel_size, y * codel_size);
+    for x in x..(x + codel_size) {
+        for y in y..(y + codel_size) {
+            let pixel = img.get_pixel(x, y);
+            if base != pixel {
+                return Err((x, y));
+            }
+        }
+    }
+    Ok(base)
+}
 
 pub fn read_code(file: &str, codel_size: u32) {
     assert!(codel_size > 0);
     let img = bmp::open(file).unwrap_or_else(|e| {
                                                  panic!("Failed to open bmp file: {}", e);
                                              });
-
     let (width, height) = (img.get_width(), img.get_height());
     if (width % codel_size != 0) | (height % codel_size != 0) {
         panic!("Bad codel size: {} ({}x{})", codel_size, width, height);
