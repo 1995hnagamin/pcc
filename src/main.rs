280291e5e499182c32ebbc6657e067039718d541
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-04-01 21:37:10 +0900

    Add fn print_usage
    Copy from the document of getopts:
    https://doc.rust-lang.org/getopts/getopts/index.html

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+extern crate getopts;
+
+use getopts::Options;
+
+
+fn print_usage(program: &str, opts: Options) {
+    let brief = format!("Usage: {} [options] FILE", program);
+    print!("{}", opts.usage(&brief));
+}
+
 fn main() {
     println!("Hello, world!");
 }
