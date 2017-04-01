a57b4f4c01f7c4849d70f96663f7a3a08565d960
NAGAMINE Hideaki <1995.hnagamin@gmail.com>
2017-04-02 02:15:28 +0900

    Add make_matrix to bmpcode.rs

--- src/bmpcode.rs
+++ src/bmpcode.rs
@@ -18,6 +18,26 @@ fn get_codel(img: &bmp::Image, x: u32, y: u32, codel_size: u32) -> ResultCodel {
     Ok(base)
 }
 
+fn make_matrix(img: &bmp::Image, codel_size: u32) {
+    let (width, height) = (img.get_width(), img.get_height());
+    if (width % codel_size != 0) | (height % codel_size != 0) {
+        panic!("Bad codel size: {} ({}x{})", codel_size, width, height);
+    };
+    let (wpixels, hpixels) = (width / codel_size, height / codel_size);
+    let matrix: Vec<Vec<bmp::Pixel>> = (0..wpixels)
+        .map(|x| {
+            (0..hpixels)
+                .map(|y| match get_codel(img, codel_size, x, y) {
+                         Ok(result) => result,
+                         Err((bx, by)) => {
+                    panic!("Bad pixel: {}x{}", bx, by);
+                }
+                     })
+                .collect()
+        })
+        .collect();
+}
+
 pub fn read_code(file: &str, codel_size: u32) {
     assert!(codel_size > 0);
     let img = bmp::open(file).unwrap_or_else(|e| {
